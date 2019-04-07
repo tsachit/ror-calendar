59d7bbc35f68780a7d6be0076723b76b8d0ec580
Sachit Karki <rockstarsachit@gmail.com>
2019-04-07 21:03:57 +0545

    authorization complete

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :authorize_request, except: [:register, :login]
-  before_action :find_user, except: %i[register index]
+  before_action :find_user, except: %i[register login]
 
   # POST /user/register
   def register
@@ -15,14 +15,21 @@ class Api::V1::UsersController < ApplicationController
 
   # POST /user/login
   def login
+    if !login_param_errors.empty?
+      render json: login_param_errors, status: :unauthorized
+      return
+    end
     @user = User.find_by_email(params[:email])
-    if @user&.authenticate(params[:password])
-      token = JsonWebToken.encode(user_id: @user.id)
-      time = Time.now + 24.hours.to_i
-      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
-                     username: @user.username }, status: :ok
+
+    if @user
+      if @user.authenticate(params[:password])
+        token = JsonWebToken.encode(@user.as_json(only: [:id, :username, :email]))
+        render json: {success: true, token: token}, status: :ok
+      else
+        render json: { password: 'Incorrect Password' }, status: :unauthorized
+      end
     else
-      render json: { error: 'unauthorized' }, status: :unauthorized
+      render json: { email: 'User not found' }, status: :not_found
     end
   end
 
@@ -64,7 +71,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def find_user
-    @user = User.find_by_username!(params[:_username])
+    @user = User.find_by_email!(params[:email])
     rescue ActiveRecord::RecordNotFound
       render json: { errors: 'User not found' }, status: :not_found
   end
@@ -79,7 +86,17 @@ class Api::V1::UsersController < ApplicationController
     params.permit(:username, :email, :password, :password_confirmation)
   end
 
-  def login_params
-    params.permit(:email, :password)
+  def login_param_errors
+    errors = {}
+    params[:email] = params[:email].strip
+    if params[:email] == ""
+      errors[:email] = "Email can't be blank"
+    end
+
+    if params[:password] == ""
+      errors[:password] = "Password can't be blank"
+    end
+
+    return errors
   end
 end
--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,13 +1,18 @@
 class JsonWebToken
   SECRET_KEY = Rails.application.secrets.secret_key_base. to_s
 
-  def self.encode(payload, exp = 24.hours.from_now)
+  def self.encode(payload = {})
+    exp= 24.hours.from_now
+    payload[:iat] = Time.now.to_i
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
+    JWT.encode(payload, SECRET_KEY, 'HS256')
   end
 
   def self.decode(token)
-    decoded = JWT.decode(token, SECRET_KEY)[0]
-    HashWithIndifferentAccess.new decoded
+    decoded = JWT.decode(token, SECRET_KEY,
+      true, algorithm: 'HS256')
+    HashWithIndifferentAccess.new(decoded)
+  rescue JWT::ExpiredSignature
+    nil
   end
 end
