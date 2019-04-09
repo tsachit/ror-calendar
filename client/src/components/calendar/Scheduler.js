b81264657b7020e80e179db4c94ae36a52bf483d
Sachit Karki <rockstarsachit@gmail.com>
2019-04-09 10:04:14 +0545

    submit button trigger

--- client/src/components/calendar/Scheduler.js
+++ client/src/components/calendar/Scheduler.js
@@ -14,8 +14,13 @@ class Scheduler extends Component {
     this.handleClose = this.handleClose.bind(this);
 
     this.state = {
-      showScheduleCreator: props.showScheduleCreator
+      showScheduleCreator: props.showScheduleCreator,
+      title: "",
+      errors: {}
     };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   handleClose() {
@@ -26,33 +31,51 @@ class Scheduler extends Component {
     this.setState({ showScheduleCreator: true });
   }
 
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newSchedule = {
+      title: this.state.title,
+      startDateTime: this.state.startDateTime,
+      endDateTime: this.state.endDateTime
+    };
+
+    console.log("============================", newSchedule);
+  }
+
   componentWillReceiveProps(nextProps) {
     this.setState({ showScheduleCreator: nextProps.showScheduleCreator });
   }
 
   render() {
-    const { showScheduleCreator } = this.state;
+    const { showScheduleCreator, errors } = this.state;
     return (
       <Modal show={showScheduleCreator} onHide={this.handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <h4>Centered Modal</h4>
-          {/* <TextFieldGroup
-            placeholder="Title"
-            name="title"
-            type="text"
-            value={this.state.title}
-            onChange={this.onChange}
-            error={errors.title}
-          /> */}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={this.props.onHide}>Close</Button>
-        </Modal.Footer>
+        <form onSubmit={this.onSubmit} noValidate>
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Modal heading
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <TextFieldGroup
+              placeholder="Title"
+              name="title"
+              type="text"
+              value={this.state.title}
+              onChange={this.onChange}
+              error={errors.title}
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <input type="submit" className="btn btn-info" />
+            <Button onClick={this.handleClose}>Close</Button>
+          </Modal.Footer>
+        </form>
       </Modal>
     );
   }
