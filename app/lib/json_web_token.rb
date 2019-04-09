4c93dae6b2ccce0f3c40e4a3dced16a31eea7a41
Sachit Karki <rockstarsachit@gmail.com>
2019-04-09 23:07:49 +0545

    jwt decoding fixed

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -11,7 +11,7 @@ class JsonWebToken
   def self.decode(token)
     decoded = JWT.decode(token, SECRET_KEY,
       true, algorithm: 'HS256')
-    HashWithIndifferentAccess.new(decoded)
+    HashWithIndifferentAccess.new(decoded[0])
   rescue JWT::ExpiredSignature
     nil
   end
