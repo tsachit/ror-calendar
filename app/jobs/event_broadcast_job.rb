319b0e1b47d85ebd96cfaec1388390ee83b1e4d5
Sachit Karki <rockstarsachit@gmail.com>
2019-04-15 07:41:00 +0545

    event broad cast for response

---
+++ app/jobs/event_broadcast_job.rb
@@ -0,0 +1,18 @@
+class EventBroadcastJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    @invitation = args[0]
+    if @invitation.status != 0
+
+      statusType = "rejected"
+      if @invitation.status == 1
+        statusType = "accepted"
+      end
+    
+      message = "#{@invitation.email} has #{statusType} to your invitation for #{@invitation.schedule.title}"
+      ActionCable.server.broadcast "activity_channel-#{@invitation.schedule.user_id}", json: { message: message }
+    end
+  end
+  
+end
--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,5 +1,6 @@
 class Invitation < ApplicationRecord
   belongs_to :schedule
+  after_update_commit { EventBroadcastJob.perform_later self }
 
   validates :email, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
